51b8ac90b1f7c0cb0773686529d3ca541621fdd6
cr0manty <aappland@gmail.com>
2020-08-24 16:39:27 +0300

    Initial Commit

---
+++ weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by Денис on 24.08.2020.
+//  Copyright © 2020 cr0manty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
